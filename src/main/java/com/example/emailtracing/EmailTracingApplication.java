384d1c968455f58bca48ead19ec7621ca10b3fb2
Phong Nguyen <nhp1807@gmail.com>
2024-03-23 20:36:10 +0700

    Create repo

---
+++ src/main/java/com/example/emailtracing/EmailTracingApplication.java
@@ -0,0 +1,13 @@
+package com.example.emailtracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailTracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailTracingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/emailtracing/EmailTracingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emailtracing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailTracingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
